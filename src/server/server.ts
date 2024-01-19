0b04f6b7801acc3dd8b04f997e6b573246326b23
LeoGomes0 <leonardobrgomes@outlook.com>
2024-01-19 15:02:03 -0300

    feat: criação do arquivo Router

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,13 @@
-import express from "express"
+import express, { json } from "express"
+import Route from "../app/routes/routes";
 
 const app = express();
 
-app.use(express());
+
+app.use(json());
+app.use(Route);
+
+
 
 app.listen(3000, () => {
     console.log("Server Online!")
