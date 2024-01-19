375dc73cefccca8e4b8229c54d1dec6e418a4e2b
LeoGomes0 <leonardobrgomes@outlook.com>
2024-01-19 17:22:56 -0300

    feat: implementando rotas ao projeto

--- src/app/controllers/ProdutoController/ProdutoController.ts
+++ src/app/controllers/ProdutoController/ProdutoController.ts
@@ -1,2 +1,76 @@
 import prisma from "../../../prisma";
+import { Request, Response } from "express";
 
+const ProdutoController = {
+
+    createProduto: async (req: Request, res: Response) => {
+        try {
+            const novoProduto = await prisma.produtos.create(req.body);
+            res.json(novoProduto);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    },
+
+    getAllProdutos: async (req: Request, res: Response) => {
+        try {
+            const produtos = await prisma.produtos.findMany();
+            res.json(produtos);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    },
+
+    getProdutoById: async (req: Request, res: Response) => {
+        try {
+            const produto = await prisma.produtos.findUnique({
+                where: {
+                    id_produto: +req.params.id
+                }
+            });
+            if (!produto) {
+                return res.status(404).send('Produto não encontrado');
+            }
+            res.json(produto);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    },
+
+    updateProduto: async (req: Request, res: Response) => {
+        try {
+            const produto = await prisma.produtos.findUnique({
+                where: {
+                    id_produto: +req.params.id
+                }
+            })
+            if (!produto) {
+                return res.status(404).send('Produto não encontrado');
+            }
+            await prisma.produtos.update(req.body);
+            res.send('Produto atualizado com sucesso');
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    },
+
+    deteleProduto: async (req: Request, res: Response) => {
+        try {
+            const produto = await prisma.produtos.findUnique({
+                where: {
+                    id_produto: +req.params.id
+                }
+            });
+            if (!produto) {
+                return res.status(404).send('Produto não encontrato');
+            }
+            prisma.produtos.delete;
+            res.send('Produto deletado com sucesso');
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    }
+
+}
+
+export default ProdutoController;
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,7 +1,16 @@
 import { Router } from "express";
+import ProdutoController from "../controllers/ProdutoController/ProdutoController";
 
 const Route = Router();
 
+Route.post('/produtos', ProdutoController.createProduto);
 
+Route.get('/produtos', ProdutoController.getAllProdutos);
+
+Route.get('/produtos/:id', ProdutoController.getProdutoById);
+
+Route.put('/produtos/:id', ProdutoController.updateProduto);
+
+Route.delete('/produtos/:id', ProdutoController.deteleProduto);
 
 export default Route;
