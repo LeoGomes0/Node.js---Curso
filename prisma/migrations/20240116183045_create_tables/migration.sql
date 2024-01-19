5eee7d7d4a1890b283359a03d61e5e4c2c86b3c8
LeoGomes0 <leonardobrgomes@outlook.com>
2024-01-19 13:45:40 -0300

    Configuração da modelagem completa

---
+++ prisma/migrations/20240116183045_create_tables/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE `Produtos` (
+    `id_produto` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome_produto` VARCHAR(191) NOT NULL,
+    `descricao` VARCHAR(191) NOT NULL,
+    `preco_unitario` DECIMAL(65, 30) NOT NULL,
+
+    PRIMARY KEY (`id_produto`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Entradas_Estoque` (
+    `id_entrada` INTEGER NOT NULL AUTO_INCREMENT,
+    `ProdutoId` INTEGER NOT NULL,
+    `quantidade` INTEGER NOT NULL,
+    `data_entrada` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id_entrada`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Saidas_Estoque` (
+    `id_saida` INTEGER NOT NULL,
+    `ProdutoId` INTEGER NOT NULL,
+    `quantidade` INTEGER NOT NULL,
+    `data_saida` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id_saida`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Entradas_Estoque` ADD CONSTRAINT `Entradas_Estoque_ProdutoId_fkey` FOREIGN KEY (`ProdutoId`) REFERENCES `Produtos`(`id_produto`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Saidas_Estoque` ADD CONSTRAINT `Saidas_Estoque_ProdutoId_fkey` FOREIGN KEY (`ProdutoId`) REFERENCES `Produtos`(`id_produto`) ON DELETE RESTRICT ON UPDATE CASCADE;
