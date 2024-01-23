b9f180825b5dee9c8a09b75a0334d1026fdb702c
LeoGomes0 <leonardobrgomes@outlook.com>
2024-01-23 20:26:37 -0300

    refactor: refatorar: refatoração da porta

--- src/app/routes/saidaRoutes.ts
+++ src/app/routes/saidaRoutes.ts
@@ -4,7 +4,7 @@ import SaidaController from "../controllers/ProdutoController/SaidaController";
 const Route = Router();
 
 // Rota para registrar saída de estoque
-Route.post('/produtos/:id/saida', SaidaController.registerSaida);
+Route.post('/saida', SaidaController.registerSaida);
 
 
 export default Route;
